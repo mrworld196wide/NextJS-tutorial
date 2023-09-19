ec0cb32d75b1838ac6f9c7f8975827f802a82624
mrworld196wide <shubhum19@gmail.com>
2023-09-20 00:59:54 +0530

    client and server side rendering

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from 'react'
+
+const AddToCart = () => {
+  return (
+    <div>
+        <button onClick={()=> alert("Hello! I am an alert box!!") }>gadi leve ghoda leve</button>
+    </div>
+  )
+}
+
+export default AddToCart
---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+import AddToCart from './AddToCart'
+const ProductCard = () => {
+  return (
+    <div>
+        <h1>Hello Ji sasri akaal ji</h1>
+        <AddToCart/>
+    </div>
+  )
+}
+export default ProductCard
